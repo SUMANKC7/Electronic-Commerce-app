b9211ae3f0e6c3fcc6182b580489d4639d71e8ff
SUMANKC7 <sumankc844@gmail.com>
2024-10-25 09:52:37 +0545

    Homepage some items are added and carousel slider is made

---
+++ lib/features/product.dart
@@ -0,0 +1,149 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class ProductView extends StatelessWidget {
+  ProductView({super.key});
+
+  List<Product> allproduct = [
+    Product(
+        image: "assests/images/phone.png",
+        productName: "Samsung S24 Ultra",
+        price: "Rs 164000", newPrice: 'Rs 154000'),
+    Product(
+        image: "assests/images/fridge.png",
+        productName: "Samsung Refrigerator 200Ltr",
+        price: "Rs 48000", newPrice: 'Rs 44000'),
+    Product(
+        image: "assests/images/laptop.png",
+        productName: "HP Vostro",
+        price: "Rs 90000", newPrice: 'Rs 80000'),
+    Product(
+        image: "assests/images/smartwatch.png",
+        productName: "Apple Watch 3",
+        price: "Rs 58000", newPrice: 'Rs 40000'),
+    Product(
+        image: "assests/images/television.png",
+        productName: "LG O-Led",
+        price: "Rs 98000", newPrice: 'Rs 67000'),
+    Product(
+        image: "assests/images/washingmachine.png",
+        productName: "Samsung Washing Machine (12 kg)",
+        price: "Rs 87000", newPrice: 'Rs 80000'),
+    Product(
+        image: "assests/images/phone.png",
+        productName: "Samsung S24 Ultra",
+        price: "Rs 164000", newPrice: 'Rs 140000'),
+    Product(
+        image: "assests/images/fridge.png",
+        productName: "Samsung Refrigerator 200Ltr",
+        price: "Rs 48000", newPrice: 'Rs 39999'),
+    Product(
+        image: "assests/images/laptop.png",
+        productName: "HP Vostro",
+        price: "Rs 90000", newPrice: 'Rs 79999'),
+    Product(
+        image: "assests/images/smartwatch.png",
+        productName: "Apple Watch 3",
+        price: "Rs 58000", newPrice: 'Rs 49999'),
+    Product(
+        image: "assests/images/television.png",
+        productName: "LG O-Led",
+        price: "Rs 98000", newPrice: 'Rs 89999'),
+    Product(
+        image: "assests/images/washingmachine.png",
+        productName: "Samsung WashingMachine (12 kg)",
+        price: "Rs 87000", newPrice: 'Rs 7777'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        padding: EdgeInsets.only(left: 20, right: 20),
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          childAspectRatio: 0.8,
+        ),
+        itemCount: allproduct.length,
+        itemBuilder: (context, index) {
+          return ProductCard(
+            allproduct: allproduct[index],
+          );
+        });
+  }
+}
+
+class Product {
+  final String image;
+  final String productName;
+  final String price;
+  final String newPrice;
+
+  Product(
+      {required this.image, required this.productName,  required this.price,required this.newPrice});
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+    required this.allproduct,
+  });
+  final Product allproduct;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // color: const Color.fromARGB(255, 249, 220, 224),
+      // color: const Color.fromARGB(255, 247, 225, 250),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          children: [
+            Expanded(
+              child: Image.asset(
+                allproduct.image,
+                fit: BoxFit.contain,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              allproduct.productName,
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10,right: 8),
+              child: Column(
+                
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(allproduct.newPrice,style: TextStyle(fontWeight: FontWeight.w900,color: const Color.fromARGB(255, 210, 128, 4),fontSize: 17),textAlign: TextAlign.left,),
+                      Container(
+                        decoration: BoxDecoration(color: Colors.orange,borderRadius: BorderRadius.circular(5)),
+                        child: Icon(Icons.add,size: 26,),
+                      )
+                    ],
+                  ),
+                  Text(allproduct.price,style: const TextStyle(decoration: TextDecoration.lineThrough,color: Colors.grey,fontSize: 13),)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,9 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:electronic_ecommerce/features/product.dart';
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
-  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
-  Homepage({super.key});
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
 
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+  List<Categories> categories = [
+    Categories("Refrigerator ", "assests/images/fridge.png"),
+    Categories("Laptop ", "assests/images/laptop.png"),
+    Categories("Phone ", "assests/images/phone.png"),
+    Categories("Smartwatch", "assests/images/smartwatch.png"),
+    Categories("TV ", "assests/images/television.png"),
+    Categories("Washing Machine", "assests/images/washingmachine.png"),
+  ];
+
+  List carosulImage = [
+    "assests/carosuelImage/deal1.png",
+    "assests/carosuelImage/deal2.png",
+    "assests/carosuelImage/deal3.webp",
+    "assests/carosuelImage/deal4.jpg",
+    "assests/carosuelImage/deal5.jpg",
+    "assests/carosuelImage/banner.png",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,11 +38,11 @@ class Homepage extends StatelessWidget {
               _globalKey.currentState?.openDrawer();
             },
             icon: const Icon(Icons.menu)),
-        title: Text("Home"),
+        title: const Text("Home"),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_bag)),
-          SizedBox(
+          IconButton(onPressed: () {}, icon:const Icon(Icons.notifications)),
+          IconButton(onPressed: () {}, icon:const Icon(Icons.shopping_bag)),
+          const SizedBox(
             width: 10,
           )
         ],
@@ -29,53 +53,217 @@ class Homepage extends StatelessWidget {
           child: ListView(
             children: [
               DrawerHeader(
-                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Colors.green
-                ),
-                child: Column(
-                  children: [
-                    Image.asset("assests/images/file.png",width: MediaQuery.of(context).size.width*0.25,),
-                    Text("Suman KC",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w900),)
-                  ],
-                )
-                ),
-             ListTile(
-              leading: Icon(Icons.wallet),
-              title: Text("Order"),
-              onTap: (){},
-             ),
-
-             ListTile(
-              leading: Icon(Icons.message),
-              title: Text("Message"),
-              onTap: (){},
-             ),
-
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  decoration: BoxDecoration(color: Colors.green),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        "assests/images/file.png",
+                        width: MediaQuery.of(context).size.width * 0.25,
+                      ),
+                      Text(
+                        "Suman KC",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w900),
+                      )
+                    ],
+                  )),
               ListTile(
-              leading: Icon(Icons.favorite),
-              title: Text("Favorite"),
-              onTap: (){},
-             ),
-
+                leading: Icon(Icons.wallet),
+                title: Text("Order"),
+                onTap: () {},
+              ),
               ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("Setting"),
-              onTap: (){},
-             ),
-
+                leading: Icon(Icons.message),
+                title: Text("Message"),
+                onTap: () {},
+              ),
               ListTile(
-              leading: Icon(Icons.account_box),
-              title: Text("Account"),
-              onTap: (){},
-             ),
+                leading: Icon(Icons.favorite),
+                title: Text("Favorite"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Setting"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.account_box),
+                title: Text("Account "),
+                onTap: () {},
+              ),
             ],
           ),
-
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.sizeOf(context).height*0.1,
+              padding:
+                  const EdgeInsets.only(left: 20, right: 20, top: 15, bottom: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 30,
+                  ),
+                  hintText: " Search Anything...",
+                  hintStyle: TextStyle(
+                      fontSize: 19,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 25, right: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Categories",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Text("View All",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.grey)),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Colors.grey,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height*0.15,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: categories.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return CategoryItem(category: categories[index]);
+                  }),
+            ),
         
-
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.15,
+              child: CarouselItem(carosulImage: carosulImage),
+            ),
+        
+            
+        
+             Padding(
+              padding: EdgeInsets.only(left: 25, right: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Top Deals",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Text("All Products",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.grey)),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Colors.grey,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          
+          ProductView()
+        
+          ],
         ),
       ),
     );
   }
+}
+
+class CarouselItem extends StatelessWidget {
+  const CarouselItem({
+    super.key,
+    required this.carosulImage,
+  });
+
+  final List<dynamic> carosulImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselView(
+        itemExtent: MediaQuery.sizeOf(context).width*0.8,
+        children: List.generate(carosulImage.length, (int index) {
+          return Image.asset(carosulImage[index],fit: BoxFit.cover,);
+        })
+        );
+  }
+}
+
+class CategoryItem extends StatelessWidget {
+  final Categories category;
+  const CategoryItem({
+    super.key,
+    required this.category,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric( horizontal: 10),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            child: Image.asset(
+              category.categoryimages,
+              width: 43,
+              height: 45,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 3,
+          ),
+          Text(
+            category.categoryName,
+            style:
+                TextStyle(color: Colors.black54, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Categories {
+  final String categoryimages;
+  final String categoryName;
+
+  Categories(this.categoryName, this.categoryimages);
 }
