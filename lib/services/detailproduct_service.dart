42edfdc38cf27e13bbc9ca35517c2d4a12336efd
SUMANKC7 <sumankc844@gmail.com>
2024-10-26 22:46:54 +0545

    Data fetching from the API

---
+++ lib/services/detailproduct_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:electronic_ecommerce/model/detailproductmodel.dart';
+import 'package:electronic_ecommerce/model/productmodel.dart';
+import 'package:http/http.dart' as http;
+
+class DetailproductService {
+  Future<List<Detailproductmodel>> getDetailProductData() async {
+    List<Detailproductmodel> storeDetailProduct = [];
+    String baseURL = "https://dummyjson.com/products/category/";
+    try {
+      var response = await http.get(Uri.parse(baseURL + "smartphones"));
+      if (response.statusCode == 200) {
+        var decodeData = jsonDecode(response.body);
+
+        for (var product in decodeData["products"]) {
+          storeDetailProduct.add(Detailproductmodel.fromJson(product));
+        }
+        return storeDetailProduct;
+      } else {
+        throw Exception("failed to load the product. Code is not 200");
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
